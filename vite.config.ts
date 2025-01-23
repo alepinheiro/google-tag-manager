c2e7e6d055bc24cdba799488c6c9292774aa1566
Ale Pinheiro <alessandropsbra@gmail.com>
2025-01-23 15:26:40 -0300

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteCompression from 'vite-plugin-compression'
 import { VitePWA } from 'vite-plugin-pwa'
-import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
   plugins: [
     vue(),
-    viteCompression({
-      // algorithm: 'brotliCompress',
-      // ext: '.br',
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
+    viteCompression(),
     VitePWA({
       registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico,txt}'],
-      },
       manifest: {
         name: 'Seu Aplicativo',
         short_name: 'App',
         theme_color: '#ffffff',
       },
-    }),
-    visualizer({
-      filename: './stats.html',
-      open: false,
     }),
   ],
   resolve: {
@@ -37,15 +24,6 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        },
-      },
-    },
     minify: 'terser',
     terserOptions: {
       compress: {
@@ -53,8 +31,5 @@ export default defineConfig({
         drop_debugger: true,
       },
     },
-  },
-  optimizeDeps: {
-    include: ['vue'],
   },
 })
