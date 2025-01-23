e29eb82b141b9ad35af9d27db0a971295a9cc931
Ale Pinheiro <alessandropsbra@gmail.com>
2025-01-23 15:56:32 -0300

    feat: add consent banner

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,8 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import ConsentBanner from './components/ConsentBanner.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import ConsentBanner from '@/components/ConsentBanner.vue'
 </script>
 
 <style scoped>
