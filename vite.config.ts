eddaf4db62ad96f164b69c96bc1da3aeef0e6cb6
Ale Pinheiro <alessandropsbra@gmail.com>
2025-01-23 15:24:08 -0300

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,60 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import viteCompression from 'vite-plugin-compression'
+import { VitePWA } from 'vite-plugin-pwa'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    viteCompression({
+      // algorithm: 'brotliCompress',
+      // ext: '.br',
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico,txt}'],
+      },
+      manifest: {
+        name: 'Seu Aplicativo',
+        short_name: 'App',
+        theme_color: '#ffffff',
+      },
+    }),
+    visualizer({
+      filename: './stats.html',
+      open: false,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['vue'],
   },
 })
