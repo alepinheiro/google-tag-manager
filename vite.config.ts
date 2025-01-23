37d30ede57fde89fa6e9c92ab76fab03baf67522
Ale Pinheiro <alessandropsbra@gmail.com>
2025-01-23 15:31:03 -0300

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
-import viteCompression from 'vite-plugin-compression'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import vercel from 'vite-plugin-vercel'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteCompression(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Seu Aplicativo',
-        short_name: 'App',
-        theme_color: '#ffffff',
-      },
-    }),
-  ],
+  plugins: [vue(), vercel(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 500,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
     },
   },
 })
